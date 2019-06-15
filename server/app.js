1473aacfb292b4512631062b4c9c4dbbe8caaee6
Nikita Gorin <nikita.gorin@rt.ru>
2019-06-15 17:29:17 +0300

    create shema

--- server/app.js
+++ server/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
+const schema = require('./shema.js');
 
 const app = express();
 const PORT = 5005;
 
-app.use('/graphql', graphqlHTTP({}));
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true,
+}));
 
 app.listen(PORT, err => {
     err ? console.log(err) : console.log('Server started!');
